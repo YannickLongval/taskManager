ddb8979d0e154bf965adb522946a4ea34216adca
Yannick Longval <yannicklongval2014@gmail.com>
2022-01-31 12:24:01 -0500

    new database

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,8 +1,15 @@
 <?php
 
-$dbServername = "localhost";
-$dbUsername = "root";
-$dbPassword = "";
-$dbName = "taskmanager";
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"], 1);
 
-$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+
+// $dbServername = "localhost";
+// $dbUsername = "root";
+// $dbPassword = "";
+// $dbName = "taskmanager";
+
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
